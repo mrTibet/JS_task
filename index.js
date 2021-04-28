b1473b90759001d27d6733ecc44935fc95390ee0
MrTibet <vader.ctylxy@gmail.com>
2021-04-28 22:48:02 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+
+//////
+let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+let arr = [];
+function randomArr(){
+    for(let i = 0; i<5;i++){
+        arr.push(characters[randomIndex()])
+    }
+    console.log(arr);
+}
+
+function randomIndex () {
+    return Math.floor(Math.random()*characters.length)
+}
+
+let letters = document.getElementById('letters');
+
+function createRandomList (){
+    for(let i = 0;i<letters.length;i++){
+        letters.children[i].innerHTML=arr[i];
+        letters.children[i].value=arr[i];
+    }
+}
+
+
+
+randomArr();
+createRandomList();
+
+letters.addEventListener('click',function(ev){
+    if(ev.target.tagName==='OPTION'){
+      console.log(ev.target.innerHTML)
+    }
+})
+
